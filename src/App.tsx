1ae924c9957eb60370d63be9627ccfb14d1e35a1
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-14 17:06:23 +0900

    [ chore ] : theme.ts 생성 및 ThemeProvider 적용 및 예시코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { DefaultTheme, ThemeProvider, styled } from "styled-components";
 import GlobalStyle from "./styles/globalStyle";
+import theme from "./styles/theme";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      초기상태
+      <ThemeProvider theme={theme}>
+        <PinterestTitleWrapper>PinterestTitleHeader</PinterestTitleWrapper>
+      </ThemeProvider>
     </>
   );
 }
 
 export default App;
+
+interface IProps {
+  theme: DefaultTheme;
+}
+
+const PinterestTitleWrapper = styled.h1<IProps>`
+  color: ${({ theme }) => theme.colors.pinterest_red};
+  ${({ theme }) => theme.fonts.pinterest_title};
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,36 @@
+import { css, DefaultTheme } from "styled-components";
+
+const colors = {
+  pinterest_black: "#000000",
+  pinterest_red: "#E60022",
+  pinterest_gray: "#EFEFEF",
+  pinterest_lightgray: "#EDEDED",
+};
+
+const fonts = {
+  pinterest_title: css`
+    font-family: Roboto;
+    font-size: 3.5rem;
+    font-weight: 600;
+    font-style: normal;
+  `,
+  pinterest_header1: css`
+    font-family: Roboto;
+    font-size: 2rem;
+    font-weight: 600;
+    font-style: normal;
+  `,
+  pinterest_header2: css`
+  font-family: Roboto;
+  font-size: 1.7rem;
+  font-weight: 500;
+  font-style: normal;
+`,
+};
+
+const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
+
+export default theme;
