e5987a50abc441b158aacea70bac65717d1d7356
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-16 17:28:26 +0900

    [ feat ] : 검색창 UI 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { styled } from "styled-components";
 import pinterestLogo from "../assets/images/pinterestLogo.png";
-import dropDownBtn from "../assets/images/down_btn.png";
+import dropDownBtn from "../assets/images/btn_down.png";
+import readingGlasses from "../assets/images/readingGlasses.png";
 function Home() {
   return (
     <>
@@ -12,6 +13,13 @@ function Home() {
             <img src={dropDownBtn} alt="dropDownBtn"></img>
           </button>
         </NavSection>
+        <SearchSection>
+          <input placeholder="핀 검색"></input>
+          <img src={readingGlasses} alt="readingGlasses" />
+        </SearchSection>
+        {/* <ManageSection>
+
+        </ManageSection> */}
       </HeaderBarWrapper>
     </>
   );
@@ -47,10 +55,10 @@ const NavSection = styled.nav`
     gap: 0.7rem;
     padding: 0;
 
-    ${({ theme }) => theme.fonts.pinterest_header1};
     border: none;
     background-color: transparent;
     color: ${({ theme }) => theme.colors.pinterest_black};
+    ${({ theme }) => theme.fonts.pinterest_header1};
     font-size: 1.6rem;
 
     img {
@@ -58,5 +66,39 @@ const NavSection = styled.nav`
       height: 0.5rem;
       margin: 0;
     }
+  }
+`;
+
+const SearchSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  width: 70%;
+
+  border: 2px solid pink;
+
+  input {
+    width: 100%;
+    height: 100%;
+    padding: 1.4rem 4.2rem;
+    margin: 0 4rem 0 2.7rem;
+
+    border: none;
+    border-radius: 2.2rem;
+    background: #e9e9e9;
+
+    ${({ theme }) => theme.fonts.pinterest_header2};
+    font-size: 1.6rem;
+  }
+  img {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 4.427rem;
+
+    width: 1.685rem;
+    height: 1.691rem;
   }
 `;
