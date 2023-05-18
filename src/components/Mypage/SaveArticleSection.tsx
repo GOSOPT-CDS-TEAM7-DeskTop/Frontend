a6ed5d0549caa711c4b1ba22c23b5dc453aeb4cf
yehyeon son <dara0615@ewhain.net>
2023-05-19 03:57:01 +0900

    [ feat ] : SaveArticleSection 컴포넌트 생성

---
+++ src/components/Mypage/SaveArticleSection.tsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+const SaveArticleSection = () => {
+  return (
+    <SaveArticleSectionWrapper>
+    SaveArticleSection
+    </SaveArticleSectionWrapper>
+
+  )
+}
+
+export default SaveArticleSection
+
+const SaveArticleSectionWrapper = styled.section`
+
+`;
