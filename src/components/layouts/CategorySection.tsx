ae160f73daa223136e0ad1a78cf7fb7d9b803387
yehyeon son <dara0615@ewhain.net>
2023-05-18 17:56:24 +0900

    [ feat ] : CategorySection 저장됨 underline 구현

--- src/components/layouts/CategorySection.tsx
+++ src/components/layouts/CategorySection.tsx
@@ -4,8 +4,8 @@ const CategorySection = () => {
   return (
     <CategorySectionWrapper>
       <CreatedStoredWrapper>
-        <div>생성됨</div>
-        <div>저장됨</div>
+        <Created>생성됨</Created>
+        <Stored>저장됨</Stored>
       </CreatedStoredWrapper>
     </CategorySectionWrapper>
   );
@@ -23,11 +23,31 @@ const CategorySectionWrapper = styled.section`
 const CreatedStoredWrapper = styled.div`
   display: flex;
   width: 100%;
+  height: 2.6rem;
 
   flex-direction: row;
   justify-content: center;
   align-items: center;
 
+
   gap: 3rem;
   margin-bottom: 0.8rem;
+`;
+
+const Created = styled.div`
+  height: 2.6rem;
+  color: ${({ theme }) => theme.colors.pinterest_black};
+  ${({ theme }) => theme.fonts.pinterest_header2};
+
+`;
+
+const Stored = styled.div`
+  height: 2.6rem;
+  color: ${({ theme }) => theme.colors.pinterest_black};
+  ${({ theme }) => theme.fonts.pinterest_header2};
+  border-bottom: 0.3rem ;
+  border-bottom-style: solid;
+  border-color: ${({ theme }) => theme.colors.pinterest_black};
+  padding-bottom: 0.8rem;
+  
 `;
