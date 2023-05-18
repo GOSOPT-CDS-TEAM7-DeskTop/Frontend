fdc6e9923aaa16ff7648cb0d5a2399d6f9915e07
yehyeon son <dara0615@ewhain.net>
2023-05-18 23:34:28 +0900

    [ feat ] : QuestionMark 컴포넌트 생성

---
+++ src/components/common/QuestionMark.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const QuestionMark = () => {
+  return <QuestionMarkWrapper>QuestionMark</QuestionMarkWrapper>;
+};
+
+export default QuestionMark;
+
+const QuestionMarkWrapper = styled.div``;
