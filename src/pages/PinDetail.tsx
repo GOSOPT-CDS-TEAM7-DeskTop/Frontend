502d16131c8b178a038c17196c950b577d45ca2a
KwonET <dlxl3652zz@gmail.com>
2023-05-23 20:39:29 +0900

    [ feat ] : 상세페이지 UI 및 컴포넌트 구현 완료

--- src/pages/PinDetail.tsx
+++ src/pages/PinDetail.tsx
@@ -9,12 +9,15 @@ import linkImg from "../assets/pinDetail/icon_link.svg";
 import saveImg from "../assets/pinDetail/icon_save.svg";
 import profileImg from "../assets/pinDetail/icon_owners_profile.png";
 import dropdownImg from "../assets/pinDetail/icon_dropdown.svg";
+import myprofileImg from "../assets/pinDetail/icon_my_profile.svg";
+import emojiImg from "../assets/pinDetail/icon_emoji.svg";
 
 const PinDetail = () => {
   return (
     <>
       <HeaderBar />
       <PinDetailWrapper>
+        {/* 왼쪽 이미지 부분 */}
         <PinImgSection>
           <BackSpan src={backSpanImg} alt="backSpan" />
           <PinImgBtns>
@@ -26,8 +29,11 @@ const PinDetail = () => {
           </PinImgBtns>
           <PinImage src={pinImg} alt="pinImage" />
         </PinImgSection>
+
+        {/* 오른쪽 내용 부분 */}
         <PinContentSection>
           <PinContentContainer>
+            {/* 오른쪽 상단 저장 부분 */}
             <PinSaveBox>
               <EtcButtons>
                 <img src={moreImg} alt="viewImg" />
@@ -40,6 +46,7 @@ const PinDetail = () => {
               </DropDownButton>
               <SaveButton>저장</SaveButton>
             </PinSaveBox>
+            {/* 오른쪽 상단 팔로우 부분 */}
             <PinFollowBox>
               <PinFollowProfile>
                 <ProfileImage src={profileImg} alt="profileImg"></ProfileImage>
@@ -51,7 +58,22 @@ const PinDetail = () => {
               <FollowButton>팔로우</FollowButton>
             </PinFollowBox>
           </PinContentContainer>
-          <PinReplContainer></PinReplContainer>
+
+          {/* 오른쪽 하단 댓글 부분 */}
+          <PinReplContainer>
+            <PinReplNumBox>
+              <p>댓글 n개</p>
+              <img src={dropdownImg} alt="dropdownImg" />
+            </PinReplNumBox>
+            <PinReplsBox></PinReplsBox>
+            <PinReplInputBox>
+              <PinReplMyProfile src={myprofileImg} alt="myprofileImg" />
+              <ReplInput>
+                <Input type="text" placeholder="댓글 추가"></Input>
+                <EmojiImg src={emojiImg} alt="emojiImg" />
+              </ReplInput>
+            </PinReplInputBox>
+          </PinReplContainer>
         </PinContentSection>
       </PinDetailWrapper>
     </>
@@ -140,9 +162,7 @@ const PinContentContainer = styled.div`
 `;
 const PinSaveBox = styled.div`
   display: flex;
-
   width: 100%;
-
   margin: 5rem 7rem 0rem 3.6rem;
 `;
 const EtcButtons = styled.div`
@@ -175,6 +195,7 @@ const DropDownButton = styled.div`
     margin-left: 0.6rem;
   }
 `;
+
 // 추후 SaveButton 컴포넌트로 대체 예정
 const SaveButton = styled.div`
   display: flex;
@@ -244,4 +265,81 @@ const FollowButton = styled.div`
   line-height: 28px;
   color: #000000;
 `;
-const PinReplContainer = styled.div``;
+
+/**
+ *  오른쪽 하단 댓글 부분
+ * */
+const PinReplContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const PinReplNumBox = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 9.8rem 0rem 0rem 2.8rem;
+  & > p {
+    display: flex;
+    align-items: center;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 2.4rem;
+    line-height: 2.8rem;
+
+    color: #000000;
+  }
+  & > img {
+    margin: 1.33rem 1rem;
+  }
+`;
+const PinReplsBox = styled.div`
+  width: 100%;
+  height: 39rem;
+`;
+
+// 댓글 입력 부분
+const PinReplInputBox = styled.div`
+  display: flex;
+
+  width: 70rem;
+  height: 10rem;
+  border: 1px solid #efefef;
+`;
+const PinReplMyProfile = styled.img`
+  width: 6.4rem;
+  height: 6.4rem;
+
+  margin: 1.8rem 3.6rem 3.2rem 1.8rem;
+`;
+const ReplInput = styled.div`
+  position: relative;
+  margin: 1.8rem 7rem 1.8rem 0rem;
+`;
+const Input = styled.input`
+  position: absolute;
+  width: 50rem;
+  height: 6.4rem;
+  padding-left: 40px;
+
+  background: #e9e9e9;
+  border-radius: 5rem;
+
+  border: none;
+
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 23px;
+
+  color: #9297a2;
+`;
+
+const EmojiImg = styled.img`
+  position: absolute;
+
+  width: 3.2rem;
+  height: 3.2rem;
+
+  margin: 1.6rem 2.3rem 1.6rem 44.5rem;
+`;
