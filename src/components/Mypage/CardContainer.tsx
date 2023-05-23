ff6b3649601fa135b4cc756060401857467d2f3c
yehyeon son <dara0615@ewhain.net>
2023-05-24 04:47:34 +0900

    [ feat ] : category_img svg->png 수정 및 alt 타입 지정

--- src/components/Mypage/CardContainer.tsx
+++ src/components/Mypage/CardContainer.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 interface CardContainerProps {
   imgSrc: string;
+  alt: string;
   title: string;
   info: string[];
 }
--- src/components/Mypage/CategorySection.tsx
+++ src/components/Mypage/CategorySection.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { styled } from "styled-components";
-import CardContainer from './CardContainer';
+import CardContainer from "./CardContainer";
 
 const CategorySection = () => {
   return (
@@ -15,45 +15,53 @@ const CategorySection = () => {
       </FilterSection>
       <CardSection>
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_1.svg" alt="card_image_1" 
+          imgSrc="../../src/assets/images/category_img/category_img_1.png"
+          alt="card_image_1"
           title="모든 핀"
           info={["핀 124개", "13주"]}
-          />   
+        />
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_2.svg" alt="card_image_2"
+          imgSrc="../../src/assets/images/category_img/category_img_2.png"
+          alt="card_image_2"
           title="해커톤"
           info={["핀 16개", "13주"]}
-           /> 
+        />
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_3.svg" alt="card_image_3" 
+          imgSrc="../../src/assets/images/category_img/category_img_3.png"
+          alt="card_image_3"
           title="랜딩페이지"
           info={["핀 17개", "21주"]}
-          />
+        />
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_4.svg" alt="card_image_4" 
+          imgSrc="../../src/assets/images/category_img/category_img_4.png"
+          alt="card_image_4"
           title="프레젠테이션"
           info={["핀 12개", "24주"]}
-          />
+        />
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_5.svg" alt="card_image_5" 
+          imgSrc="../../src/assets/images/category_img/category_img_5.png"
+          alt="card_image_5"
           title="팜플렛"
           info={["핀 3개", "25주"]}
-          />
+        />
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_6.svg" alt="card_image_6" 
+          imgSrc="../../src/assets/images/category_img/category_img_6.png"
+          alt="card_image_6"
           title="명함 디자인"
           info={["핀 17개", "21주"]}
-          />
+        />
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_7.svg" alt="card_image_7" 
+          imgSrc="../../src/assets/images/category_img/category_img_7.png"
+          alt="card_image_7"
           title="UI 디자인"
           info={["핀 17개", "35주"]}
-          />
+        />
         <CardContainer
-          imgSrc="../../src/assets/images/category_img_8.svg" alt="card_image_8" 
+          imgSrc="../../src/assets/images/category_img/category_img_8.png"
+          alt="card_image_8"
           title="대시보드 디자인"
           info={["핀 18개", "21주"]}
-          />
+        />
       </CardSection>
       <Line />
     </CategorySectionWrapper>
@@ -108,7 +116,7 @@ const FilterSection = styled.span`
   flex-direction: row;
   justify-content: space-between;
 
-  width:180.8rem;
+  width: 180.8rem;
 
   margin-bottom: 2.4rem;
 
@@ -121,53 +129,16 @@ const FilterSection = styled.span`
 const CardSection = styled.section`
   display: flex;
   flex-direction: row;
-  
+
   justify-content: center;
 
   width: 180.8rem;
 
   gap: 1.6rem;
 
-  img {
-    width: 21.2rem;
-    height: 14rem;
-
-    border-radius: 1.2rem;
-  }
 `;
 
-/*
-const CardContainer = styled.div`
-  position: relative;
 
-  width: 21.2rem;
-  height: 14rem;
-
- 
-
-  h2 {
-    position: absolute;
-    top: 60%;
-    left: 6.6%;
-
-    color: ${({ theme }) => theme.colors.pinterest_white};
-    ${({ theme }) => theme.fonts.pinterest_header1};
-  }
-`;
-*/
-
-const CardInfo = styled.span`
-  display: flex;
-  position: absolute;
-
-  top: 80.71%;
-  left: 6.6%;
-
-  gap: 0.4rem;
-
-  color: ${({ theme }) => theme.colors.pinterest_white};
-  ${({ theme }) => theme.fonts.pinterest_header4};
-`;
 
 const Line = styled.div`
   width: 100%;
