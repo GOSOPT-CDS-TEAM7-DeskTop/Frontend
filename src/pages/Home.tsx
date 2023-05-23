f0d324fb5cdfe3f833ebbc9c6e50b5d0a3d7ef47
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-23 20:59:30 +0900

    [ test ] : 전체게시물 가져오기 api test code 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,23 @@
 import { styled } from "styled-components";
 import MasonryLayout from "../components/layouts/MasonryLayout";
 import HeaderBar from "../components/layouts/HeaderBar";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Home() {
+  const getAllArticle = async () => {
+    try {
+      const res = await axios.get("http://13.209.7.118:8080/pin");
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllArticle();
+  });
+
   return (
     <>
       <HeaderBar />
