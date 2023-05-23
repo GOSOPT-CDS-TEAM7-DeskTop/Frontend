4c0d7c2a570d3e46065f756337fc74be9171d2e9
yehyeon son <dara0615@ewhain.net>
2023-05-24 03:30:23 +0900

    [ feat ] : CardContainer 컴포넌트 분리

--- src/components/Mypage/CategorySection.tsx
+++ src/components/Mypage/CategorySection.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { styled } from "styled-components";
+import CardContainer from './CardContainer';
 
 const CategorySection = () => {
   return (
@@ -12,70 +14,46 @@ const CategorySection = () => {
         <img src="../../src/assets/icon/plus_icon.svg" alt="plus_icon" />
       </FilterSection>
       <CardSection>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_1.svg" alt="card_image_1" />
-          <h2>모든 핀</h2>
-          <CardInfo>
-            <p>핀 124개</p>
-            <p>13주</p>
-          </CardInfo>
-        </CardContainer>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_2.svg" alt="card_image_2" />
-          <h2>해커톤</h2>
-          <CardInfo>
-            <p>핀 16개</p>
-            <p>13주</p>
-          </CardInfo>
-        </CardContainer>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_3.svg" alt="card_image_3" />
-          <h2>랜딩페이지</h2>
-          <CardInfo>
-            <p>핀 17개</p>
-            <p>21주</p>
-          </CardInfo>
-        </CardContainer>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_4.svg" alt="card_image_4" />
-          <h2>프레젠테이션</h2>
-          <CardInfo>
-            <p>핀 12개</p>
-            <p>24주</p>
-          </CardInfo>
-        </CardContainer>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_5.svg" alt="card_image_5" />
-          <h2>팜플렛</h2>
-          <CardInfo>
-            <p>핀 3개</p>
-            <p>25주</p>
-          </CardInfo>
-        </CardContainer>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_6.svg" alt="card_image_6" />
-          <h2>명함 디자인</h2>
-          <CardInfo>
-            <p>핀 17개</p>
-            <p>21주</p>
-          </CardInfo>
-        </CardContainer>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_7.svg" alt="card_image_7" />
-          <h2>UI 디자인</h2>
-          <CardInfo>
-            <p>핀 17개</p>
-            <p>35주</p>
-          </CardInfo>
-        </CardContainer>
-        <CardContainer>
-          <img src="../../src/assets/images/category_img_8.svg" alt="card_image_8" />
-          <h2>대시보드 디자인</h2>
-          <CardInfo>
-            <p>핀 18개</p>
-            <p>21주</p>
-          </CardInfo>
-        </CardContainer>
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_1.svg" alt="card_image_1" 
+          title="모든 핀"
+          info={["핀 124개", "13주"]}
+          />   
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_2.svg" alt="card_image_2"
+          title="해커톤"
+          info={["핀 16개", "13주"]}
+           /> 
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_3.svg" alt="card_image_3" 
+          title="랜딩페이지"
+          info={["핀 17개", "21주"]}
+          />
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_4.svg" alt="card_image_4" 
+          title="프레젠테이션"
+          info={["핀 12개", "24주"]}
+          />
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_5.svg" alt="card_image_5" 
+          title="팜플렛"
+          info={["핀 3개", "25주"]}
+          />
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_6.svg" alt="card_image_6" 
+          title="명함 디자인"
+          info={["핀 17개", "21주"]}
+          />
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_7.svg" alt="card_image_7" 
+          title="UI 디자인"
+          info={["핀 17개", "35주"]}
+          />
+        <CardContainer
+          imgSrc="../../src/assets/images/category_img_8.svg" alt="card_image_8" 
+          title="대시보드 디자인"
+          info={["핀 18개", "21주"]}
+          />
       </CardSection>
       <Line />
     </CategorySectionWrapper>
@@ -158,6 +136,7 @@ const CardSection = styled.section`
   }
 `;
 
+/*
 const CardContainer = styled.div`
   position: relative;
 
@@ -175,6 +154,7 @@ const CardContainer = styled.div`
     ${({ theme }) => theme.fonts.pinterest_header1};
   }
 `;
+*/
 
 const CardInfo = styled.span`
   display: flex;
