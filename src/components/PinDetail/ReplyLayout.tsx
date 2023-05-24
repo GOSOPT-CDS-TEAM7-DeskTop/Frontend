58d629857764daf1d480d7e0438ddc5b79202453
KwonET <dlxl3652zz@gmail.com>
2023-05-25 02:02:41 +0900

    [ feat ] : 날짜 차이 계산하는 함수 추가

--- src/components/PinDetail/ReplyLayout.tsx
+++ src/components/PinDetail/ReplyLayout.tsx
@@ -4,6 +4,43 @@ import heartIcon from "../../assets/icon/icon_heart.png";
 import userProfile from "../../assets/images/icon_profile_comment.png";
 
 function ReplyLayout({ commentData }) {
+
+  // 날짜 차이를 계산해주는 함수
+  const getTimeDifference = (createdTimeData) => {
+    const nowTime = new Date();
+    const createdTime = createdTimeData.replace("T", " ");
+    const dateCreatedTime = new Date(createdTime.toString());
+    // 초 (밀리초)
+    const seconds = 1;
+    // 분
+    const minute = seconds * 60;
+    // 시
+    const hour = minute * 60;
+    // 일
+    const day = hour * 24;
+    // 달
+    const month = day * 30;
+    //년
+    const year = month * 12;
+
+    var elapsedTime = Math.trunc((nowTime.getTime() - dateCreatedTime.getTime()) / 1000);
+    var elapsedText = "";
+    if (elapsedTime < seconds) {
+      elapsedText = "방금 전";
+    } else if (elapsedTime < minute) {
+      elapsedText = elapsedTime + "s";
+    } else if (elapsedTime < hour) {
+      elapsedText = Math.trunc(elapsedTime / minute) + "m";
+    } else if (elapsedTime < day) {
+      elapsedText = Math.trunc(elapsedTime / hour) + "h";
+    } else if (elapsedTime < month) {
+      elapsedText = Math.trunc(elapsedTime / day) + "d";
+    } else if (elapsedTime < year) {
+      elapsedText = Math.trunc(elapsedTime / month) + "m";
+    }
+    return elapsedText;
+  };
+  
   return (
     <>
       <ReplyWrapper>
@@ -16,7 +53,7 @@ function ReplyLayout({ commentData }) {
             </p>
           </ReplyMainContainer>
           <ReplySubContainer>
-            <small>{commentData.createdTime}</small>
+            <small>{getTimeDifference(commentData.createdTime)}</small>
             <small>답글</small>
             <img src={heartIcon} alt="heartIcon" />
             <small>{commentData.like}</small>
