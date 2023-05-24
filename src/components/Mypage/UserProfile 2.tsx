9366dabe377f05d99c2b2499562bac261564577d
KwonET <dlxl3652zz@gmail.com>
2023-05-25 03:07:42 +0900

    [ feat ] : save 여부 post

---
+++ src/components/Mypage/UserProfile 2.tsx
@@ -0,0 +1,119 @@
+import { styled } from "styled-components";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface User {
+  code: number;
+  message: string;
+  data: UserData;
+}
+
+interface UserData {
+  userImage: string;
+  nickname: string;
+  account: string;
+  following: number;
+}
+
+function UserProfile() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  const getUserData = async () => {
+    try {
+      const res = await axios.get<User>("https://team7.collab-pinterest.p-e.kr/user");
+      console.log(res.data);
+      setUserData(res.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  return (
+    <>
+      <UserProfileWrapper>
+        <UserImageSection>
+          <img src={userData?.userImage} alt="userImage" />
+        </UserImageSection>
+        <UserName>{userData?.nickname}</UserName>
+        <UserId>{userData?.account}</UserId>
+        <FollowNum>팔로잉 {userData?.following}명</FollowNum>
+        <ButtonSection>
+          <ShareButton>공유</ShareButton>
+          <ShareButton>프로필 수정</ShareButton>
+        </ButtonSection>
+      </UserProfileWrapper>
+    </>
+  );
+}
+
+export default UserProfile;
+
+const UserProfileWrapper = styled.section`
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+
+  flex-direction: column;
+
+  margin: 2.4rem;
+`;
+
+const UserImageSection = styled.div`
+  margin-bottom: 1.6rem;
+
+  img {
+    width: 11rem;
+    height: 11rem;
+
+    border-radius: 55%;
+  }
+`;
+
+const UserName = styled.h1`
+  margin-bottom: 1.2rem;
+  color: ${({ theme }) => theme.colors.pinterest_black};
+
+  ${({ theme }) => theme.fonts.pinterest_title};
+
+  font-weight: 700;
+`;
+
+const UserId = styled.h4`
+  margin-bottom: 0.8rem;
+
+  color: ${({ theme }) => theme.colors.pinterest_deepgray};
+  ${({ theme }) => theme.fonts.pinterest_header4};
+
+  font-weight: 600;
+`;
+
+const FollowNum = styled.h3`
+  color: ${({ theme }) => theme.colors.pinterest_black};
+  ${({ theme }) => theme.fonts.pinterest_header3};
+
+  font-weight: 600;
+`;
+
+const ButtonSection = styled.div`
+  display: flex;
+  gap: 0.7rem;
+`;
+
+const ShareButton = styled.button`
+  padding: 1.2rem 1.5rem;
+  margin-top: 1.6rem;
+
+  border: none;
+  border-radius: 7.3rem;
+
+  color: ${({ theme }) => theme.colors.pinterest_black};
+  background-color: ${({ theme }) => theme.colors.pinterest_gray};
+
+  ${({ theme }) => theme.fonts.pinterest_header2};
+  font-weight: 700;
+`;
