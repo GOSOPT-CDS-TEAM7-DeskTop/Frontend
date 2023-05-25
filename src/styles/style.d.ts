c668c120ffe9d2a26ed112796ffd3fb7796636a9
Dev-jwJeong <rgn95652@gmail.com>
2023-05-26 03:47:22 +0900

    [style] : blue, background 색상 추가

--- src/styles/style.d.ts
+++ src/styles/style.d.ts
@@ -6,12 +6,17 @@ declare module "styled-components" {
       pinterest_black: string;
       pinterest_red: string;
       pinterest_gray: string;
+      pinterest_blue: string;
       pinterest_lightgray: string;
+      pinterest_white: string;
+      pinterest_background: string;
     };
     fonts: {
       pinterest_title: SerializedStyles;
       pinterest_header1: SerializedStyles;
       pinterest_header2: SerializedStyles;
+      pinterest_header3: SerializedStyles;
+      pinterest_header4: SerializedStyles;
     };
   }
 }
