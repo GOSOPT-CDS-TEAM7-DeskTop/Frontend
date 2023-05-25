b4454b5e8767c1f11ecf25ff1619d02f988c1523
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-26 03:52:31 +0900

    [ feat ] : DefaultTheme type 지정

--- src/styles/style.d.ts
+++ src/styles/style.d.ts
@@ -1,17 +1,21 @@
 import "styled-components";
 
 declare module "styled-components" {
-  export interface IDefaultTheme {
+  export interface DefaultTheme {
     colors: {
       pinterest_black: string;
       pinterest_red: string;
+      pinterest_deepgray: string;
       pinterest_gray: string;
       pinterest_lightgray: string;
+      pinterest_white: string;
     };
     fonts: {
       pinterest_title: SerializedStyles;
       pinterest_header1: SerializedStyles;
       pinterest_header2: SerializedStyles;
+      pinterest_header3: SerializedStyles;
+      pinterest_header4: SerializedStyles;
     };
   }
 }
