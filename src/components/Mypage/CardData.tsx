26f33ad87f50602c24e2cd02bd31f7c653f8208b
yehyeon son <dara0615@ewhain.net>
2023-05-26 02:34:44 +0900

    [ refactor ] :  cardData 이미지 import 방식으로 수정

--- src/components/Mypage/CardData.tsx
+++ src/components/Mypage/CardData.tsx
@@ -1,53 +1,61 @@
-const CardData = [
-    {
-      imgSrc: "../../src/assets/images/category_img/category_img_1.png",
-      alt: "card_image_1",
-      title: "모든 핀",
-      info: ["핀 124개", "13주"],
-    },
-    {
-      imgSrc: "../../src/assets/images/category_img/category_img_2.png",
-      alt: "card_image_2",
-      title: "해커톤",
-      info: ["핀 16개", "13주"],
-    },
-    {
-        imgSrc: "../../src/assets/images/category_img/category_img_3.png",
-        alt: "card_image_3",
-        title: "랜딩페이지",
-        info: ["핀 17개", "21주"],
-    },
-    {
-        imgSrc: "../../src/assets/images/category_img/category_img_4.png",
-        alt: "card_image_4",
-        title: "프레젠테이션",
-        info: ["핀 12개", "24주"],
-    },
-    {
-        imgSrc: "../../src/assets/images/category_img/category_img_5.png",
-        alt: "card_image_5",
-        title: "팜플렛",
-        info: ["핀 3개", "25주"],
-    },
-    {
-        imgSrc: "../../src/assets/images/category_img/category_img_6.png",
-        alt: "card_image_6",
-        title: "명함 디자인",
-        info: ["핀 17개", "21주"],
-    },
-    {
-        imgSrc: "../../src/assets/images/category_img/category_img_7.png",
-        alt: "card_image_7",
-        title: "UI 디자인",
-        info: ["핀 17개", "35주"],
-    },
-    {
-        imgSrc: "../../src/assets/images/category_img/category_img_8.png",
-        alt: "card_image_8",
-        title: "대시보드 디자인",
-        info: ["핀 18개", "21주"],
+import category_img_1 from "../../assets/images/category_img/category_img_1.png";
+import category_img_2 from "../../assets/images/category_img/category_img_2.png";
+import category_img_3 from "../../assets/images/category_img/category_img_3.png";
+import category_img_4 from "../../assets/images/category_img/category_img_4.png";
+import category_img_5 from "../../assets/images/category_img/category_img_5.png";
+import category_img_6 from "../../assets/images/category_img/category_img_6.png";
+import category_img_7 from "../../assets/images/category_img/category_img_7.png";
+import category_img_8 from "../../assets/images/category_img/category_img_8.png";
 
-    }
-  ];
-  
-  export default CardData;
+const CardData = [
+  {
+    imgSrc: category_img_1,
+    alt: "card_image_1",
+    title: "모든 핀",
+    info: ["핀 124개", "13주"],
+  },
+  {
+    imgSrc: category_img_2,
+    alt: "card_image_2",
+    title: "해커톤",
+    info: ["핀 16개", "13주"],
+  },
+  {
+    imgSrc: category_img_3,
+    alt: "card_image_3",
+    title: "랜딩페이지",
+    info: ["핀 17개", "21주"],
+  },
+  {
+    imgSrc: category_img_4,
+    alt: "card_image_4",
+    title: "프레젠테이션",
+    info: ["핀 12개", "24주"],
+  },
+  {
+    imgSrc: category_img_5,
+    alt: "card_image_5",
+    title: "팜플렛",
+    info: ["핀 3개", "25주"],
+  },
+  {
+    imgSrc: category_img_6,
+    alt: "card_image_6",
+    title: "명함 디자인",
+    info: ["핀 17개", "21주"],
+  },
+  {
+    imgSrc: category_img_7,
+    alt: "card_image_7",
+    title: "UI 디자인",
+    info: ["핀 17개", "35주"],
+  },
+  {
+    imgSrc: category_img_8,
+    alt: "card_image_8",
+    title: "대시보드 디자인",
+    info: ["핀 18개", "21주"],
+  },
+];
+
+export default CardData;
